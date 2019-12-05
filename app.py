234dac2a283f97dd8f2df9c105b0f8303edf4959
To Chin Yu <ytc@stanford.edu>
2019-12-04 16:08:43 -0800

    use heroku sql

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly
 import model
+import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -12,7 +13,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div(
-    html.Div([        
+    html.Div([
         dcc.Graph(id='live-update-graph'),
         dcc.Interval(
             id='interval-component',
@@ -25,20 +26,16 @@ app.layout = html.Div(
 @app.callback(Output('live-update-graph', 'figure'),
               [Input('interval-component', 'n_intervals')])
 def update_graph_live(n):
-    temp6_data = list(zip(*model.read_channel('temp6', 8000)))
-    temp8_data = list(zip(*model.read_channel('temp8', 8000)))
-
     fig = plotly.subplots.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
-    fig.append_trace({
-        'x': list(temp6_data[0]),
-        'y': list(temp6_data[1]),
-        'name': 'T6'
-    }, 1, 1)
-    fig.append_trace({
-        'x': list(temp8_data[0]),
-        'y': list(temp8_data[1]),
-        'name': 'T8'
-    }, 1, 1)
+
+    channels = model.get_channels()
+    for c in channels:
+        data = model.read_channel(c,8000)
+        fig.append_trace({
+            'x': list(data[0]),
+            'y': list(data[1]),
+            'name': c
+        }, 1 if 'temp' in c else 2, 1)
 
     return fig
 
--- model.py
+++ model.py
@@ -1,27 +1,40 @@
 import mysql.connector
 import time
 import os
+import itertools
 
-def read_channel(chan,num_entries,timespan=None):
-    db_connection = mysql.connector.Connect(user=os.environ['CDMS_DB_USER'],password=os.environ['CDMS_DB_PWD'],host=os.environ['CDMS_DB_HOST'],port=3307,database=os.environ['CDMS_DB_NAME'])
+def get_channels():
+    db_connection = mysql.connector.Connect(
+        user=os.environ['CDMS_DB_USER'], password=os.environ['CDMS_DB_PWD'], host=os.environ['CDMS_DB_HOST'], port=3306, database=os.environ['CDMS_DB_NAME'])
+    db_cursor = db_connection.cursor(buffered=True)
+
+    db_cursor.execute('''
+    SELECT DISTINCT channel
+    FROM bluefors''')
+
+    results = db_cursor.fetchall()
+    db_cursor.close()
+    db_connection.close()
+    return list(itertools.chain(*results))
+
+
+def read_channel(chan, num_entries, timespan=None):
+    db_connection = mysql.connector.Connect(
+        user=os.environ['CDMS_DB_USER'], password=os.environ['CDMS_DB_PWD'], host=os.environ['CDMS_DB_HOST'], port=3306, database=os.environ['CDMS_DB_NAME'])
     db_cursor = db_connection.cursor(buffered=True)
 
     db_cursor.execute(f'''
     SELECT
-    test_db.fridgeTime,test_db.lakeshore372_{chan}
-    FROM test_db
+    fridgeTime,reading
+    FROM bluefors
     WHERE fridgeTime >= NOW() - INTERVAL 1 DAY
-    ORDER BY test_db.fridgeTime DESC''')
+    AND channel = "{chan}"
+    ORDER BY fridgeTime DESC''')
 
-    if num_entries>0:
+    if num_entries > 0:
         results = db_cursor.fetchmany(num_entries)
     else:
         results = db_cursor.fetchall()
     db_cursor.close()
     db_connection.close()
-    return results
-
-
-
-
-
+    return list(zip(*results))
